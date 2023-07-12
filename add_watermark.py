9b32f5e51028ec5cef6146eaad6331b54d7e376d
Mati55e <matic.trcko@gmail.com>
2023-07-12 16:35:36 +0200

    First Commit

---
+++ add_watermark.py
@@ -0,0 +1,62 @@
+from PIL import Image, ImageDraw, ImageFont
+from PIL.ImageTk import PhotoImage
+
+
+class WatermarkEngine():
+    def __init__(self, path):
+        self.text = "Sample Text"
+        self.path = path
+        self.base_image = Image.open(self.path, mode="r")
+        self.tk_image = PhotoImage(self.base_image.convert("RGB"))
+        self.w, self.h = self.base_image.size
+
+    def text_watermark(self):
+        try:
+            text_img = Image.new("RGBA", (self.w, self.h), (255, 255, 255, 0))
+            draw = ImageDraw.Draw(text_img)
+            font = ImageFont.truetype('fonts/Roboto/Roboto-Bold.ttf', 36)
+
+            ascent, descent = font.getmetrics()
+            text_w = font.getmask(self.text).getbbox()[2]
+            text_h = font.getmask(self.text).getbbox()[3] + descent
+
+            margin = 15
+            x = self.w - text_w - margin
+            y = self.h - text_h - margin
+
+            draw.text((x, y), text=self.text, font=font, fill=(255, 255, 255, 80))
+            self.output = Image.alpha_composite(self.base_image.convert(mode="RGBA"), text_img)
+            self.tk_image = PhotoImage(self.output.convert(mode="RGB"))
+
+        except OSError:
+            print("Could not open image. Check image extension.")
+
+    # def img_watermark(self):
+    #     watermark = Image.open("./watermark_images/watermark.png", mode="r")
+    #     watermark.show()
+    #     wm_w, wm_h = watermark.size
+    #     coef = self.w / wm_w
+    #     watermark = watermark.resize((self.w, int(wm_h*coef)))
+    #     watermark.putalpha(90)
+    #     self.output = Image.new(mode="RGBA", size=(self.w, self.h))
+    #     self.output.paste(self.base_image)
+    #     y_pos = int((self.h / 2) - ((wm_h) / 2 * coef))
+    #     self.output.paste(watermark, (0, y_pos), watermark)
+    #     self.tk_image = PhotoImage(self.output.convert(mode="RGB"))
+
+    def img_watermark(self):
+        watermark = Image.open("./watermark_images/watermark.png", mode="r")
+        wm_w, wm_h = watermark.size
+        coef = self.w / wm_w
+        watermark = watermark.resize((self.w, int(wm_h * coef)))
+        watermark.putalpha(90)
+        base_watermark = Image.new(mode="RGBA", color=(0,0,0,0), size=(self.w, self.h))
+        y_pos = int((self.h / 2) - ((wm_h) / 2 * coef))
+        base_watermark.paste(watermark, (0, y_pos))
+        self.output = Image.alpha_composite(self.base_image.convert(mode="RGBA"), base_watermark)
+        self.tk_image = PhotoImage(self.output.convert(mode="RGB"))
+
+    def save(self, path):
+        print(path)
+        self.output = self.output.convert(mode="RGB")
+        self.output.save(fp=f"{path}.jpg")
---
+++ main.py
@@ -0,0 +1,71 @@
+import os.path
+from tkinter import Tk, Label, Button, Canvas
+from add_watermark import WatermarkEngine
+from tkinter import filedialog as fd
+from tkinter import ttk
+
+
+
+def get_img_path():
+
+    # file = fd.askopenfile(mode="r", filetypes=[("PNG", "*png"), ("JPG", "*.jpg")])
+    global file_path
+    # file_path = file.name
+    file_path = "./playnite-png.png"
+
+    selected_file_label = Label(frm2, text="Your selected file: " + file_path)
+    selected_file_label.pack(side="top", anchor="nw")
+
+    global g_image
+    g_image = WatermarkEngine(path=file_path)
+    global canvas
+    canvas = Canvas(frm2, width=g_image.w, height=g_image.h)
+    canvas.pack(anchor="nw")
+    display_img()
+
+
+def save_img():
+    save_path = fd.asksaveasfilename()
+    g_image.save(path=save_path)
+
+
+def display_img():
+    canvas.create_image(10, 10, anchor="nw", image=g_image.tk_image)
+
+
+def text_watermark():
+    g_image.text_watermark()
+    canvas.create_image(10, 10, anchor="nw", image=g_image.tk_image)
+
+
+def img_watermark():
+    select_watermark_img_button = Button(frm, text="Select watermark image")
+    g_image.img_watermark()
+    canvas.create_image(10, 10, anchor="nw", image=g_image.tk_image)
+
+
+root = Tk()
+root.title("Watermarking App")
+root.geometry("900x600")
+
+frm = ttk.Frame(root)
+frm2 = ttk.Frame(root)
+frm.pack(anchor="nw")
+frm2.pack(anchor="nw")
+
+label = Label(frm, text="Add a watermark to your image")
+search_file_button = Button(frm, text="Select base image", command=get_img_path)
+save_button = Button(frm, text="Save watermarked image", command=save_img)
+text_watermark_button = Button(frm, text="Apply text watermark", command=text_watermark)
+img_watermark_button = Button(frm, text="Apply watermark with an image", command=img_watermark)
+
+label.pack(anchor="nw")
+search_file_button.pack(side="left", anchor="nw")
+text_watermark_button.pack(side="left", anchor="nw")
+img_watermark_button.pack(side="left", anchor="nw")
+save_button.pack(side="top", anchor="nw")
+
+get_img_path()
+
+frm.mainloop()
+
